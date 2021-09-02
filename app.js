c75883c7b785d679af95f1fe93e18e820892fa9c
gideonekekeke <71523437+gideonekekeke@users.noreply.github.com>
2021-09-02 15:03:31 +0100

    first vercel

---
+++ app.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const port = 3000;
+
+const callRoute = require("./Router");
+const url =
+  "mongodb+srv://shotkode:shotkode@cluster0.2kfdg.mongodb.net/RomanusDB?retryWrites=true&w=majority";
+
+const app = express();
+
+mongoose.connect(url, { useNewUrlParser: true }).then(() => {
+  console.log("successfully connected");
+});
+
+app.use(express.json());
+app.get("", (req, res) => {
+  res.send("server is been deployed");
+});
+app.use("/api", callRoute);
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
+
+// try {
+//   functionThatMightThrow();
+// } catch (error) {
+//   notifyUserOfError(error);
+//   reportErrorToService(error);
+// }
